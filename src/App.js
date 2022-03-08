41eb426d5019c64fd0a32b3a88fbe3447a126030
luisval16 <eduardoc1112@gmail.com>
2022-03-08 01:02:43 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,20 @@
 import Inicio from "./inicio";
 import Encabezado from "./Encabezado";
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route} from 'react-router-dom';
 import Contacto from "./Contacto";
 import Productos from "./Productos";
 
 function App() {
-    return ( <
-        >
-        <
-        Encabezado / >
-        <
-        Routes >
-        <
-        Route path = "/"
-        element = { < Inicio / > }
-        /> <
-        Route path = "/contacto"
-        element = { < Contacto / > }
-        /> <
-        Route path = "/productos"
-        element = { < Productos / > }
-        /> <
-        /Routes> <
-        />
-    );
+  return (
+    <>
+    <Encabezado/>
+    <Routes>
+      <Route path="/" element={<Inicio/>} />
+      <Route path="/contacto" element={<Contacto/>} />
+      <Route path="/productos" element={<Productos/>}/>
+    </Routes>
+    </>
+  );
 }
 
 export default App;
